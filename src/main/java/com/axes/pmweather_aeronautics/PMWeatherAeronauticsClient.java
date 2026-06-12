6dbd9123f2ca55f1d782d2770c5b3110961498da
G9third <42447809+G9third@users.noreply.github.com>
2026-06-12 13:24:03 -0400

    Add NeoForge config screen registration

---
+++ src/main/java/com/axes/pmweather_aeronautics/PMWeatherAeronauticsClient.java
@@ -0,0 +1,14 @@
+package com.axes.pmweather_aeronautics;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+
+@Mod(value = PMWeatherAeronautics.MODID, dist = Dist.CLIENT)
+public final class PMWeatherAeronauticsClient {
+    public PMWeatherAeronauticsClient(final ModContainer modContainer) {
+        modContainer.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+    }
+}
